440109aa0b0c55d49a6c82a7f436555f90b37e3c
faiisu <sukumdecha@gmail.com>
2025-12-05 09:41:34 +0000

    fix build state conflict

--- frontend/src/components/CampaignCategoriesModal.tsx
+++ frontend/src/components/CampaignCategoriesModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaLayerGroup, FaTrash, FaTimes, FaCheck, FaBars } from 'react-icons/fa';
+import { FaLayerGroup, FaTimes, FaCheck, FaBars } from 'react-icons/fa';
 import {
     DndContext,
     closestCenter,
